ea3ac1ba300de3f7c8203596b0dd3727c6893603
Varsha Kumar <varsha.kumar@outlook.com>
2017-03-03 20:24:39 -0600

    made all code in last year's goal class comments, cleaned up and added comments in Config

--- src/com/Team5427/res/Config.java
+++ src/com/Team5427/res/Config.java
@@ -7,168 +7,82 @@ package com.Team5427.res;
  */
 public class Config {
 
-	///link for IP Camera M1013 https://www.axis.com/files/manuals/um_axis_M1013_63352_en_1509.pdf
-	//This is the real one 
-	
-	//vals for auto shoot
-	public static final double SHOOT_MIN_SLOPE=-1;
-	public static final double SHOOT_MAX_SLOPE=-1;
-
-	
-
-	/**
-	 * Number of targets, will be used if we use the same code for another game
-	 */
-	public static final int NUM_OF_TARGETS = 2;
-
-	public static final String NAME = "Team5427RoboCode";
-
-	public static final boolean DEBUG_MODE = false; // displays 'Log.debug' in
-													// console
-	public static final boolean LOGGING = true;
-
-	public static final boolean BYTE_SENDER_THREAD_ENABLE = true;
-
+	/**---------IMPORTANT SOFTWARECONSTANTS----------*/
+	//Values that we need for interaction with other programs, or for the GraphicsPanel
+	/**link for IP Camera M1013 https://www.axis.com/files/manuals/um_axis_M1013_63352_en_1509.pdf*/
+	//This is the real IP camera address one
 	//public static final String DEFAULT_CAMERA_IP = "http://10.54.27.13/mjpg/video.mjpg";
 	public static final String DEFAULT_CAMERA_IP = "http://169.254.101.224/mjpg/video.mjpg";
-
+	/** Number of targets, will be used if we use the same code for another game*/
+	public static final int NUM_OF_TARGETS = 2;
+	public static final String NAME = "Team5427RoboCode";
+	public static final boolean DEBUG_MODE = false; // displays 'Log.debug' in console
+	public static final boolean LOGGING = true;
+	public static final boolean BYTE_SENDER_THREAD_ENABLE = true;	
 	
-	
-	/**
-	 * Determines whether or not the user will be able to calibrate the camera
-	 * from the GraphicsPanel.
-	 */
-	public static final boolean ENABLE_FOV_CALIBRATION = false;
-	/**
-	 * Horizontal FOV of the attached Camera
-	 */
-	//public static double horizontalFOV = 55.689320368051696;
-	public static final double HORIZONTAL_FOV = 67;
-//	public static double horizontalFOV = 75;
-	/**
-	 * Vertical FOV of the attached Camera
-	 */
-	//public static final double verticalFOV = 42.296048;
-	public static final double VERTICAL_FOV = 51;
-
-	/**
-	 * Angle at which the camera is mounted on the robot, in degrees.
-	 */
-	public static double CAMERA_START_ANGLE = 11.5;//30
-
-	
-	/**
-	 * Actual height of the tapes.
-	 */
-
-//	public static final double TOP_TAPE_HEIGHT = 4;
-//	public static final double BOTTOM_TAPE_HEIGHT = 2;
-	
-	/**
-	 * Dimensions of Resolution of Camera
-	 */
-	public static final int RESOLUTION_WIDTH = 640;
-	public static final int RESOLUTION_HEIGHT = 480;
-
-	/**
-	 *   coordinates on image so that we are lined up to shoot properly
-	 */
-	public static final int ALIGNED_LEFT_X=200;
-	public static final int ALIGNED_RIGHT_X=500;	
-
-	
-	/**
-	 * Elevation of the camera from the carpet, at the point where it is
-	 * attached to the robot.
-	 */
-	public static final double ROBOT_HEIGHT = 28.5;
-	/**
-	 * Elevation of the goal from the bottom of the reflective tape to the
-	 * carpet.
-	 */
+	/**----------PERMANENT CONSTANTS----------*/
+	//distances in inches for field distances or distances on robot
+	/** Elevation of the goal from the top of the reflective tape to the carpet.*/
 	public static final double TARGET_HEIGHT_TOP = 88;// from ground to top of top tape;
 	public static final double TARGET_HEIGHT_BOTTOM = 80;//80; from ground to top of bottom tape
-	/**Values for the equations for the theta vs. pixels line*/
-	/*private static final double ACTUAL_DISTANCE_X_1=138;
-	private static final double ACTUAL_DISTANCE_X_2=87;//TODO 2nd data point
-	private static final double TOP_THETA_1=Math.atan((TARGET_HEIGHT_TOP-ROBOT_HEIGHT)/ACTUAL_DISTANCE_X_1);
-	private static final double TOP_THETA_2=Math.atan((TARGET_HEIGHT_TOP-ROBOT_HEIGHT)/ACTUAL_DISTANCE_X_2);
-	private static final double BOTTOM_THETA_1=Math.atan((TARGET_HEIGHT_BOTTOM-ROBOT_HEIGHT)/ACTUAL_DISTANCE_X_1);
-	private static final double BOTTOM_THETA_2=Math.atan((TARGET_HEIGHT_BOTTOM-ROBOT_HEIGHT)/ACTUAL_DISTANCE_X_2);
-	private static final double TOP_PEAK_1=245;
-	private static final double TOP_PEAK_2=138;
-	private static final double BOTTOM_PEAK_1=274;
-	private static final double BOTTOM_PEAK_2=175;
-	private static final double TOP_PIXELS_1=Math.abs(RESOLUTION_HEIGHT/2-TOP_PEAK_1);
-	private static final double TOP_PIXELS_2=Math.abs(RESOLUTION_HEIGHT/2-TOP_PEAK_2);
-	private static final double BOTTOM_PIXELS_1=Math.abs(RESOLUTION_HEIGHT/2-BOTTOM_PEAK_1);
-	private static final double BOTTOM_PIXELS_2=Math.abs(RESOLUTION_HEIGHT/2-BOTTOM_PEAK_2);
-
-	//TODO make sure we align left and right first, then distance
-
-	/**Distance Equation Values
-	public static final double SLOPE_TOP_EQN= (TOP_THETA_2-TOP_THETA_1)/(TOP_PIXELS_2-TOP_PIXELS_1);
-	public static final double INTERCEPT_TOP_EQN= -TOP_PIXELS_1*SLOPE_TOP_EQN+TOP_THETA_1;
-	public static final double SLOPE_BOTTOM_EQN= (BOTTOM_THETA_2-BOTTOM_THETA_1)/(BOTTOM_PIXELS_2-BOTTOM_PIXELS_1);
-	public static final double INTERCEPT_BOTTOM_EQN= -BOTTOM_PIXELS_1*SLOPE_BOTTOM_EQN+BOTTOM_THETA_1;
-	*/
-
+	/** Elevation of the camera from the carpet, at the point where it is attached to the robot.*/
+	public static final double ROBOT_HEIGHT = 28.5;
+	/**Angle at which the camera is mounted on the robot, in degrees.*/
+	public static double CAMERA_START_ANGLE = 11.5;//30
+		
+	/*----------CAMERA VIDEO VALUES----------*/
+	//Values of FOV, or used for calibrating FOV,or Camera resolution
+	/** Horizontal FOV of the attached Camera*/
+	public static final double HORIZONTAL_FOV = 67;
+	//public static double horizontalFOV = 55.689320368051696;
+	//public static double horizontalFOV = 75;
+	/**Vertical FOV of the attached Camera */
+	public static final double VERTICAL_FOV = 51;
+	//public static final double verticalFOV = 42.296048;
+	/** Determines whether or not the user will be able to calibrate the camera
+	 * from the GraphicsPanel.*/
+	public static final boolean ENABLE_FOV_CALIBRATION = false;
+	/** Dimensions of Resolution of Camera*/
+	public static final int RESOLUTION_WIDTH = 640;
+	public static final int RESOLUTION_HEIGHT = 480;
+	
+	/*----------FILTERING CONTOURS----------*/
+	//Values used in Main to filter contours
 	/**Tolerated Difference btw. CenterX's of Correct Tape Contours**/
 	public static final int TAPE_DIF_CENTER_X=5;
 	/**Tolerated Difference btw.Widths of Correct Tape Contours**/
 	public static final int TAPE_DIF_WIDTH=5;
 	
-	
-	
-	/** The distance between the center of the turret to the camera*/
-	public static final double CAMERA_TURRET_DISTANCE = 5;
-
 	/** ------------Repaint Thread------------**/
 	// Config for VisionPanel thread repainting
 	public static final double MAX_FPS = 60f;
-
-	/**
-	 * Amount of time in autonomous in seconds
-	 */
+	/**Amount of time in autonomous in seconds */
 	public static final long AUTO_TIME = 15;
-
-	/**
-	 * Amount of time in teleop in seconds
-	 */
-
+	/** Amount of time in teleop in seconds*/
 	public static final long TELEOP_TIME = 135;
-
-	/** -----------------Legacy Code----------------- **/
-
-	/**
-	 * Actual height of the goal.
-	 */
-	public static final double TRUE_GOAL_HEIGHT = 14;
-
-	/**
-	 * Elevation of the goal from the bottom of the reflective tape to the
-	 * carpet.
-	 */
-	public static final double TOWER_HEIGHT = 85;
 	
-	
-	// Used for the range lines that print to screen
-		/**max and min distances for shooting*/
+	/**-----------PAINTING OF GUIDELINES----------*/
+	//Values used for the range lines that print to screen
+	/**coordinates on image so that we are lined up to shoot properly*/
+	public static final int ALIGNED_LEFT_X=200;
+	public static final int ALIGNED_RIGHT_X=500;
+	/**max and min distances for shooting*/
 	public static final double MAX_SHOOTING_DISTANCE=100;
 	public static final double MIN_SHOOTING_DISTANCE=80;
 	public static final int LOWEST_SHOOT_LINE =getPixelHeight(MIN_SHOOTING_DISTANCE,TARGET_HEIGHT_TOP-ROBOT_HEIGHT);
 	public static final int HIGHEST_SHOOT_LINE =getPixelHeight(MAX_SHOOTING_DISTANCE,TARGET_HEIGHT_BOTTOM-ROBOT_HEIGHT);
-	
+	/**takes a horizontal distance and a vertical distance and returns the y-value for a pixel that would show
+	 * up on the camera for that distance and height
+	 * @param horizontalDistance the distance in inches to the robot, horizontally from the goal
+	 * @param height the true height, in inches, of the goal you are aiming for
+	 * @return the pixelY-value for the given horizontalDistance
+	 */
 	public static int getPixelHeight(double horizontalDistance, double height)
 	{
-		//horizontalDistance=200;
-		//height=80-28.5;
-		
 		double theta = Math.abs(Math.atan(height/horizontalDistance));
 		double addedAngle=Math.toDegrees(theta)-CAMERA_START_ANGLE;
 		double ratio= addedAngle/((VERTICAL_FOV)/2);
 		double pixelHeight= ratio*(RESOLUTION_HEIGHT/2);
 		return (int)(Math.round(Math.abs(RESOLUTION_HEIGHT/2-pixelHeight)));
-		//return pixelHeight;
 	}
 }
