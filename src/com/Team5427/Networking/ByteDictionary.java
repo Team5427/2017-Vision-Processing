8f19c59ffd3a39e6b4bf6ae3931a8fe71575daf6
Charlemagne Wong <frian13@hotmail.com>
2017-03-09 19:14:21 -0600

    Fix byte dictionary

--- src/com/Team5427/Networking/ByteDictionary.java
+++ src/com/Team5427/Networking/ByteDictionary.java
@@ -1,43 +1,45 @@
 package com.Team5427.Networking;
 
+/**
+ * Byte dictionary to indicate the byte array type
+ */
 public class ByteDictionary {
 
+	public static final byte UNUSED = -1;
 
 	public static final byte MESSAGE = 0;
 
 	public static final byte LOG = 1;
 
-	public static final byte JOYSTICK = 2;
+	public static final byte OBJECT = 2;
 
-	public static final byte ROTATE = 3;
-	
-	public static final byte SHOOT_BUTTON = 1;
-	public static final byte PULL_BUTTON = 8;
-	public static final byte SWITCH_CAMERAS_BUTTON = 3;
-	public static final byte START_INTAKE_BUTTON = 4;
-	public static final byte CHANGE_INTAKE_DIRECTION_BUTTON = 6;
-	public static final byte SPIN_BUTTON = 2;
-//	public static final byte FLAP_REATRACTED = 7;
-//	public static final byte FLAP_GEAR = 9;
-//	public static final byte FLAP_INTAKE=11;
-	public static final byte MOVE = 4;
+	public static final byte DISABLED = 3;
 
-	public static final byte AUTO_START = -1;
-
-	public static final byte DISABLED = -1;
-
-	public static final byte TIME_REMAINING = -1;
-
-	public static final byte OBJECT = 9;
+	public static final byte TIME_REMAINING = 4;
 
 	public static final byte TELEOP_START = 5;
 
-	public static final byte SHOOT_BUTTON = 1;
-	public static final byte PULL_BUTTON = 8;
-	public static final byte SWITCH_CAMERAS_BUTTON = 3;
-	public static final byte START_INTAKE_BUTTON = 4;
-	public static final byte CHANGE_INTAKE_DIRECTION_BUTTON = 6;
-	public static final byte SPIN_BUTTON = 2;
+	public static final byte JOYSTICK = 6;
+
+	public static final byte ROTATE = 7;
+
+	public static final byte MOVE = 8;
+
+	public static final byte TARGET_DATA = 9;
+
+	public static final byte AUTO_START = 10;
+
+	public static final byte SHOOT = 11;
+
+	public static final byte CLIMB = 12;
+
+	public static final byte SWITCH_CAMERAS = 13;
+
+	public static final byte INTAKE = 14;
+
+//	public static final byte FLAP_REATRACTED = 7;
+//	public static final byte FLAP_GEAR = 9;
+//	public static final byte FLAP_INTAKE=11;
 //	public static final byte FLAP_REATRACTED = 7;
 //	public static final byte FLAP_GEAR = 12;
 //	public static final byte FLAP_INTAKE=11;
